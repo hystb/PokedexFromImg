cd1062ab53383b83ce299039903b44178b3ae29d
milan <milan.nicolas@rocketmail.com>
2024-06-26 16:58:25 +0200

    init solve

--- solve.py
+++ solve.py
@@ -1,2 +1,29 @@
 from utils import *
+from CNN import CNN
+import pickle
+import torchvision as tv
+import torch
+from main import loadModel
 
+def solve(classesPath, modelPath, device, pathToTest, batchSize=64):
+    with open(classesPath, 'rb') as f:
+        classes = pickle.load(f)
+        print(len(classes))
+        model = CNN(len(classes))
+    model = loadModel(model, modelPath, device)
+    print("Model Loaded")
+    preprocessData = tv.transforms.Compose(
+	    [tv.transforms.Lambda(lambda image: image.convert('RGB')),
+	     tv.transforms.Lambda(pad_to_square),
+	     tv.transforms.Resize((128, 128)),
+	     tv.transforms.ToTensor()]
+	    )
+    dataTrain = tv.datasets.ImageFolder(root=pathToTest, transform=preprocessData)
+    dataTrainL = torch.utils.data.DataLoader(dataTrain,
+	                                         batch_size = batchSize,
+	                                         shuffle=False)
+    testImg(dataTrainL, classes, model, device, batchSize)
+    
+    
+        
+    
