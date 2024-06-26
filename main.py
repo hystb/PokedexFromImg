8d99087feced13ba75bad9ad731bb42bb1775583
milan <milan.nicolas@rocketmail.com>
2024-06-26 16:58:09 +0200

    add solver

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import argparse
 import torch
+from CNN import CNN
 from train import *
+from solve import *
 
 def loadModel(model, model_load_path, device):
     model.load_state_dict(torch.load(model_load_path))
     model.to(device)
-    model.eval()
     return model
 
 def main():
@@ -18,21 +19,24 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Using {} device".format(device))
     if args.mode == 'train':
-        nEpoch = 50
-        batchSize = 64
+        nEpoch = 100
+        batchSize = 256
         learning_rate = 0.001
         model = None
         pathToData = "./PokemonData"
         if args.pathm:
+            model = CNN(len(dataTrain.classes))
             model = loadModel(model, args.pathm, device)
+            model.train()
             print("Model Loaded")
         if args.pathd:
             pathToData = args.pathd
         train(model=model, learningRate=learning_rate, nEpoch=nEpoch, device=device, batchSize=batchSize, pathToData=pathToData)
-    # elif args.mode == 'execute':
-    #     model = loadModel(model, args.pathm, device)
-    #     maxId = 150
-    #     if args.nclasses:
+    elif args.mode == 'execute':
+        if args.nclasses and args.pathm and args.pathd:
+            solve(args.nclasses, args.pathm, device, args.pathd)
+        else:
+            print("Missing --pathm, --pathd and/or --nclasses use --help for more informations")
             
         
 if __name__ == "__main__":
